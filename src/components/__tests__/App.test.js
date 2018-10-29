e7a6c7fc3e44e7435eb152e1a15b21d051f746c0
SoeRatch <surajkoho@gmail.com>
2018-10-29 19:47:59 +0530

    Enzyme-test suite setup done

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 it('shows a comment box',()=>{
-	const div = document.createElement('div');
-
-	ReactDOM.render(<App />, div);
-
-	//Looks inside the div 
-	//and checks to see if the CommentBox is in there
-	expect(div.innerHTML).toContain('Comment Box');
-	
-	ReactDOM.unmountComponentAtNode(div);
+	const wrapped = shallow(<App />);
+	expect(wrapped.find(CommentBox).length).toEqual(1);
 });
