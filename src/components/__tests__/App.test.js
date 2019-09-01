b35dd2944b3ac52ce7fb20295f485e6bfd190e81
SoeRatch <surajkoho@gmail.com>
2019-09-01 10:50:18 +0530

    test whether App component has CommentList instance or not.

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -7,6 +7,10 @@ import CommentList from '../CommentList';
 it('shows a comment box',()=>{
 	const wrapped = shallow(<App />);
 	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list',()=>{
+	const wrapped = shallow(<App />);
 	expect(wrapped.find(CommentList).length).toEqual(1);
 });
 
