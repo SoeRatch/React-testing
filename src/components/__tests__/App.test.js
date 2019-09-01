f759b31c76f5dd82fb70e23264a357fdfa3c3e69
SoeRatch <surajkoho@gmail.com>
2019-09-01 10:46:31 +0530

    test whether App component has instance of CommentList or not

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box',()=>{
 	const wrapped = shallow(<App />);
 	expect(wrapped.find(CommentBox).length).toEqual(1);
+	expect(wrapped.find(CommentList).length).toEqual(1);
 });
+
+/*
+At the top level enzyme give us three additional capabillities to run our test .
+
+Three general methods for creating instances of our component and writing expectation of our components .
+Three ways to interact with enzyme .
+1) Static - render the given component and return plain html
+
+2) Shallow - render just the given component and none of its children 
+
+3) Full DOM -  render the component and all of its children 
+
+documentation airbnb.io/enzyme
+
+*/
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,6 @@
+//here the name of the file should be exactly setupTests.js 
+// so when test is run enzyme first like for this file and executes this before running code in any other file
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
