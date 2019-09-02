84e93c43d6654fcd8bbf8b6bc1e6e6755659d793
SoeRatch <surajkoho@gmail.com>
2019-09-02 12:37:38 +0530

    wire up commentBox component with redux

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
 
@@ -15,9 +17,7 @@ class CommentBox extends Component {
 	handleSubmit = event => {
 		event.preventDefault(); // keeps our page from attempting to reload .
 	
-		// Call an action creator
-		// And save the comment
-
+		this.props.saveComment(this.state.comment);
 		this.setState({
 			comment:''
 		});
@@ -38,4 +38,4 @@ class CommentBox extends Component {
 	}
 }
 
-export default CommentBox;
+export default connect(null,actions)(CommentBox);
